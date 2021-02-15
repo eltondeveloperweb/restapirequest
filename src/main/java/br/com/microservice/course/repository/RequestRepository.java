ba3a031966073a26b1cf5001e70cf970202e50c2
Elton Adriano <eltonfullstack@gmail.com>
2021-02-15 10:06:56 -0300

    Repository Request and Repository RequestStage

---
+++ src/main/java/br/com/microservice/course/repository/RequestRepository.java
@@ -0,0 +1,20 @@
+package br.com.microservice.course.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.microservice.course.domain.Request;
+import br.com.microservice.course.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+	
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
+
+}
---
+++ src/main/java/br/com/microservice/course/repository/RequestStageRepository.java
@@ -0,0 +1,15 @@
+package br.com.microservice.course.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.microservice.course.domain.RequestStage;
+
+@Repository
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long>{
+
+	public List<RequestStage> findAllByRequestId(Long id);
+		
+}
